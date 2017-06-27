27e06df6642e866424ad397336fa167b770e6374
Jeremy <jgmiller88@gmail.com>
2017-06-26 22:40:26 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# portal
-Transfer data between two "portals".
+# Portal
+
+Transfers data between two colored `portals` (similar to the [Portal](https://en.wikipedia.org/wiki/Portal_(video_game)) video game).  This project is based on [this](http://howistart.org/posts/elixir/1/) tutorial.
+
+## TODO
+- ExUnit tests
+- Generate ExDoc documentation
+- Docker
+- Update readme badges
+- Build, static analysis, etc
+
+## Usage
+```
+$ iex -S mix
+iex> Portal.shoot(:orange)
+{:ok, #PID<0.72.0>}
+iex> Portal.shoot(:blue)
+{:ok, #PID<0.74.0>}
+iex> portal = Portal.transfer(:orange, :blue, [1, 2, 3, 4])
+
+       :orange <=> :blue
+  [1, 2, 3, 4] <=> []
+
+iex> Portal.push_data(portal, :right)
+
+    :orange <=> :blue
+  [1, 2, 3] <=> [4]
+
+iex> Portal.push_data(portal, :left)
+
+       :orange <=> :blue
+  [1, 2, 3, 4] <=> []
+
+```
---
+++ lib/portal.ex
@@ -0,0 +1,79 @@
+defmodule Portal do
+  @moduledoc """
+  Create a Portal with two colored doors for transferring data.
+  """
+
+  @doc """
+  The `left` and `right` doors of the Portal.
+  """
+  defstruct [:left, :right]
+
+  @doc """
+  Create a new Portal door with given `color` in the supervisor.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
+  end
+
+  @doc """
+  Setup transfer of `data` between `left` and `right` doors.
+  """
+  def transfer(left, right, data) do
+    # Add all data to the portal on the left.
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data in one direction (to the `:left` or `:right`) for the given `portal`.
+  """
+  def push_data(portal, direction) do
+    case direction do
+      :left   -> push(portal.right, portal.left)
+      :right  -> push(portal.left, portal.right)
+      _       -> :error
+    end
+
+    # Return the portal so our protocol displays it properly.
+    portal
+  end
+
+  @doc false
+  defp push(from, to) do
+    # If possible, pushes data from the `from` door to the `to` door.
+    # Otherwise, do nothing.
+    case Portal.Door.pop(from) do
+      :error        -> :ok
+      {:ok, value}  -> Portal.Door.push(to, value)
+    end
+  end
+end
+
+defimpl Inspect, for: Portal do
+  @doc """
+  Display the Portal data in a more readable way.
+
+  E.g.
+      :orange <=> :blue
+    [1, 2, 3] <=> []
+  """
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
+
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    # Use String.rjust() to make sure the columns line up.
+    """
+
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    """
+  end
+end
